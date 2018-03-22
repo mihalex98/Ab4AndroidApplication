a8e1cc1e6c6a093dcb6cd338aeaa82da098ea3c0
Mihai Tataru <mihai.tataru@student.manchester.ac.uk>
2018-03-22 15:04:44 +0000

    Initial commit

---
+++ app/src/main/java/com/ab4application/mihai/stackoverflowinformation/TopUsersActivity.java
@@ -0,0 +1,13 @@
+package com.ab4application.mihai.stackoverflowinformation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TopUsersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_users);
+    }
+}
